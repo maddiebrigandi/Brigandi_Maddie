b04132436f7d92f7de6822b5acf7957ab2fc51f2
maddiebrigandi <maddiebrigandi@gmail.com>
2016-09-06 11:13:14 -0700

    Added Files

---
+++ APLesson_01/ascii.java
@@ -0,0 +1,14 @@
+public class ascii
+{
+	public static void main(String[]args)
+	{
+		System.out.println("            |`-.__.");
+		System.out.println("            / ' _/");
+		System.out.println("           ****` ");
+		System.out.println("          /    }");
+		System.out.println("         /  \\ /");
+		System.out.println("     \\ /`   \\\\\\");
+		System.out.println("      `\\    /_\\\\");
+		System.out.println("       `~~~~~``~`");
+	}
+}
---
+++ APLesson_01/ascii2.java
@@ -0,0 +1,18 @@
+public class ascii2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("     _.-- ,.--.");
+		System.out.println("   .'   .'    /");
+		System.out.println("   | @       |'..--------._");
+		System.out.println("  /      \\._/              '.");
+		System.out.println(" /  .-.-                     \\");
+		System.out.println("(  /    \\                     \\");
+		System.out.println(" \\\\      '.                  | #");
+		System.out.println("  \\\\       \\   -.           /");
+		System.out.println("   :\\       |    )._____.'   \\");
+		System.out.println("            |   /  \\  |  \\    )");
+		System.out.println("            |   |./'  :__ \\.-'");
+		System.out.println("            '--'");
+	}
+}
