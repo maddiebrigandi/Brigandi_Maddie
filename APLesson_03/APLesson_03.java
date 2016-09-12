d04a1b906c5f8f5a7a0fb0b5faf993a5815706ef
maddiebrigandi <maddiebrigandi@gmail.com>
2016-09-12 12:15:56 -0700

    lesson 3

---
+++ APLesson_03/APLesson_03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; //import Statement
+
+public class APLesson_03
+{
+	public static void main(String[]args)
+	{
+		//insantiate the new Scanner object "keyboard"
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompt for user input
+		System.out.println("How old are you? ");
+		
+		//search for the next integer that the user enters
+		int num = keyboard.nextInt();
+		
+		//print the results...
+		System.out.println("Wow! " + num + "is perfect. You're pretty handsome");
+		
+	}
+}
