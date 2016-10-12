43d33ef1c7a9c3fb115b5e83dddb8724a20ac039
maddiebrigandi <maddiebrigandi@gmail.com>
2016-10-11 20:47:59 -0700

    Update APLab_05.java

--- APLesson_05/APLab_05.java
+++ APLesson_05/APLab_05.java
@@ -2,12 +2,13 @@ public class APLab_05
 {
 	public static void main(String[]args)
 	{
-		int player = (Math.random()*7)+1;
-		int computer = (Math.random()*7)+1;
+		int player = (Math.random()*6)+1;
+		int computer = (Math.random()*6)+1;
 		
-		String Winner = //output from diceRoll
+		String Winner = diceRoll(player, computer);//output from diceRoll
 		System.out.println("You rolled a " + player);
 		System.out.println("Computer rolled a " + computer);
+		System.out.println("And the winner is " + Winner);
 	}
 	
 	public static double diceRoll(int player, int computer)
@@ -15,10 +16,11 @@ public class APLab_05
 		String winner = "";
 		//if player is greater
 		if(player>computer)
-			winner = player;
+			winner = "player";
 		//if computer is greater
-		if(computer>player)
-			winner = computer;
+		else if(computer>player)
+			winner = "computer";
+		else winnter = "tie";
 		return winner;
 		
 	}
