520b442d35e554c8e67e067a4def8d382e4ab120
maddiebrigandi <maddiebrigandi@gmail.com>
2017-03-30 11:49:19 -0700

    finished 14.2

--- APLesson_14/14.2/Satellite2.java
+++ APLesson_14/14.2/Satellite2.java
@@ -22,7 +22,7 @@ public class Satellite2
 		   double xmove = Math.round((1+ (Math.random()*100))*100.00)/100.00;
 		   double ymove = Math.round((1+ (Math.random()*100))*100.00)/100.00;
 		   printout += "\nAfter " + l.getID() + " Moved (" + getLocation(l.getLoc()) + ")";
-           (l).move(xmove, 2*ymove);
+           (l).move(xmove, ymove);
 		   printout += "\nNew Location: (" + getLocation(l.getLoc()) + ")\n\n";
        }
 
@@ -43,12 +43,12 @@ public class Satellite2
 
    public static double getDistance(double[] car, double[] home)
    {
-       return Math.sqrt("%.2f"(Math.pow(car[0] - home[0], 2)+ Math.pow(car[0] - home[1], 2)));
+       return Math.sqrt((Math.pow((car[0] - home[0]), 2)+ Math.pow(car[0] - home[1], 2)));
    }
 
 
    public static String getLocation(double[] loc)
    {
-       return String.format(("%.2f, %.2f") loc[0] , loc[1]);
+       return loc[0] + " , " + loc[1];
    }
 }
