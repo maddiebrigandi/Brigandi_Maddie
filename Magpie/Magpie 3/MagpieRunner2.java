9d28da4d91d14b655ce567d9ddd29528189b3137
maddiebrigandi <maddiebrigandi@gmail.com>
2017-05-24 12:45:07 -0700

    Fixed Magpie

---
+++ Magpie/Magpie 3/MagpieRunner2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; 
+ 
+ 
+ /** 
+  * A simple class to run the Magpie class. 
+  * @author Laurie White 
+  * @version April 2012 
+  */ 
+ public class MagpieRunner2 
+ { 
+ 
+ 
+ 	/** 
+ 	 * Create a Magpie, give it user input, and print its replies. 
+ 	 */ 
+ 	public static void main(String[] args) 
+ 	{ 
+ 		Magpie2 maggie = new Magpie2(); 
+ 		 
+ 		System.out.println (maggie.getResponse(" ")); 
+ 		System.out.println(maggie.getResponse("I know not no.")); 
+ 		System.out.println (maggie.getResponse("My dog died.")); 
+ 		System.out.println(maggie.getResponse("My Father is lazy.")); 
+ 		System.out.println(maggie.getResponse("I want to sleep.")); 
+ 		System.out.println(maggie.getResponse("I hate you.")); 
+ 		System.out.println(maggie.getResponse("You love me.")); 
+ 
+ 
+ 	} 
+ 
+ 
+ } 
